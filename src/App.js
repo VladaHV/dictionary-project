bcdabcb3ebaf258fb7662b6a35cbb883caf6f3c5
VladaHV <110497335+VladaHV@users.noreply.github.com>
2023-10-04 13:22:06 +0300

    Search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo"></img>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo"></img>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Vlada Honchar</small>
+        </footer>
+      </div>
     </div>
   );
 }
